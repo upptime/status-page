3c3df6f2747508ead7a8ee7ab6bf47abd6bed1b6
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-14 02:29:50 +0530

    :bug: Use root config in post-process

--- post-process.ts
+++ post-process.ts
@@ -3,11 +3,20 @@ import { safeLoad } from "js-yaml";
 import { join } from "path";
 
 export const preProcess = async () => {
-  const config = safeLoad(await readFile(join(".", ".upptimerc.yml"), "utf8")) as {
+  let config: {
     "status-website"?: {
       cname?: string;
     };
-  };
+  } = safeLoad(await readFile(join(".", ".upptimerc.yml"), "utf8")) as any;
+
+  try {
+    const file = await readFile(join("..", "..", ".upptimerc.yml"), "utf8");
+    if (file) {
+      config = safeLoad(file) as any;
+      console.log("Using root config instead");
+    }
+  } catch (error) {}
+
   if (config["status-website"]?.cname)
     await writeFile(join(".", "__sapper__", "export", "CNAME"), config["status-website"]?.cname);
   await copyFile(
