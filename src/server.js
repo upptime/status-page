ef07d8e5dfffbfeb3fd2e2762860ad99eb48b04f
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-27 16:07:25 +0530

    :sparkles: Add support for custom base URLs

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
-import sirv from 'sirv';
-import polka from 'polka';
-import compression from 'compression';
-import * as sapper from '@sapper/server';
+import * as sapper from "@sapper/server";
+import compression from "compression";
+import fs from "fs";
+import polka from "polka";
+import sirv from "sirv";
+import { safeLoad } from "js-yaml";
 
 const { PORT, NODE_ENV } = process.env;
-const dev = NODE_ENV === 'development';
+const dev = NODE_ENV === "development";
+const configFile = safeLoad(fs.readFileSync(".upptimerc.yml", "utf8"));
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+polka()
+  .use(
+    (configFile || {})["status-website"].baseUrl || "/",
+    compression({ threshold: 0 }),
+    sirv("static", { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, (err) => {
+    if (err) console.log("error", err);
+  });
