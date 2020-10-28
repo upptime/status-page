50fb1973b40e6508c5d0c442754396d3f8fb8013
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-28 14:03:27 +0530

    :bug: Use correct config file (fixed upptime/upptime#40)

--- src/server.js
+++ src/server.js
@@ -1,21 +1,27 @@
 import * as sapper from "@sapper/server";
 import compression from "compression";
-import fs from "fs";
+import fs from "fs-extra";
 import polka from "polka";
 import sirv from "sirv";
 import { safeLoad } from "js-yaml";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
-const configFile = safeLoad(fs.readFileSync(".upptimerc.yml", "utf8"));
+
+let config = safeLoad(fs.readFileSync(join(".", ".upptimerc.yml"), "utf8"));
+try {
+  const file = fs.readFileSync(join("..", "..", ".upptimerc.yml"), "utf8");
+  if (file) {
+    config = safeLoad(file);
+    console.log("Using root config instead", config);
+  }
+} catch (error) {
+  console.log("Root config not found 2 dirs up");
+}
+const baseUrl = (config["status-website"] || {}).baseUrl || "/";
 
 polka()
-  .use(
-    (configFile || {})["status-website"].baseUrl || "/",
-    compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleware()
-  )
+  .use(baseUrl, compression({ threshold: 0 }), sirv("static", { dev }), sapper.middleware())
   .listen(PORT, (err) => {
     if (err) console.log("error", err);
   });
