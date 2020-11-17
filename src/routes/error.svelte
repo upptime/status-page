33f26527a76b7a4416121f54799e983f4e35ec78
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-17 21:23:07 +0530

    :sparkles: Add general error page

---
+++ src/routes/error.svelte
@@ -0,0 +1,31 @@
+<script>
+  import config from "../data/config.json";
+</script>
+
+<style>
+  p.lead {
+    font-size: 110%;
+  }
+  a.button {
+    font: inherit;
+    padding: 0.5rem 1rem;
+    border: 0.1rem solid rgba(0, 0, 0, 0.25);
+    border-radius: 0.2rem;
+    background-color: #01a3a4;
+    text-decoration: none;
+    color: #fff;
+    border-color: transparent;
+  }
+</style>
+
+<svelte:head>
+  <title>{config.i18n.errorTitle}</title>
+</svelte:head>
+
+<h1>{config.i18n.errorTitle}</h1>
+
+<p class="lead">{config.i18n.errorIntro}</p>
+
+<p>{config.i18n.errorText}</p>
+
+<a href="/" class="button">{config.i18n.errorHome}</a>
