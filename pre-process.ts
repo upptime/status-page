1c932ce180b071e9459fcf9f77692036723d5fff
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-24 00:43:00 +0530

    :recycle: Add base URL in config

--- pre-process.ts
+++ pre-process.ts
@@ -10,11 +10,19 @@ export const preProcess = async () => {
   let config: {
     logoUrl?: string;
     name?: string;
+    owner: string;
+    repo: string;
     introTitle?: string;
     introMessage?: string;
+    path: string;
     i18n?: { [index: string]: string };
+    "status-website"?: {
+      cname?: string;
+    };
   } = safeLoad(await readFile(join("..", ".upptimerc.yml"), "utf8")) as any;
-
+  if (!config.owner || !config.repo) throw new Error("Owner/repo not set");
+  config.path = `https://${config.owner}.github.io/${config.repo}/`;
+  if (config["status-website"]?.cname) config.path = `https://${config["status-website"].cname}`;
   config.i18n = { ...i18n, ...config.i18n };
   await ensureDir(join(".", "src", "data"));
   await writeJson(join(".", "src", "data", "config.json"), config);
