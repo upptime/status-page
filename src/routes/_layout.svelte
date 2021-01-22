17aa659a17e05ed3662301caf8ce75da207b41e7
Anand Chowdhary <mail@anandchowdhary.com>
2021-01-22 19:29:03 +0530

    :sparkles: Add support for multiple themes

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,51 +2,43 @@
   import Nav from "../components/Nav.svelte";
   import config from "../data/config.json";
   import snarkdown from "snarkdown";
-  import { each } from "svelte/internal";
   export let segment;
 </script>
-
-<style>
-  footer {
-    text-align: center;
-    opacity: 0.75;
-    margin-top: 3rem;
-  }
-</style>
 
 <svelte:head>
   <link
     rel="icon"
     type="image/svg"
-    href={(config['status-website'] || {}).faviconSvg || (config['status-website'] || {}).favicon || `https://raw.githubusercontent.com/koj-co/upptime/master/assets/icon.svg`} />
+    href={(config["status-website"] || {}).faviconSvg ||
+      (config["status-website"] || {}).favicon ||
+      `https://raw.githubusercontent.com/koj-co/upptime/master/assets/icon.svg`}
+  />
   <link
     rel="icon"
     type="image/png"
-    href={(config['status-website'] || {}).favicon || `/logo-192.png`} />
-  {#if config['status-website'].scripts}
-    {#each config['status-website'].scripts as script}
-      <script src={script.src} async={!!script.async} defer={!!script.async}>
-      </script>
-    {/each}
+    href={(config["status-website"] || {}).favicon || `/logo-192.png`}
+  />
+  {#if config["status-website"].scripts}
+    {#each config["status-website"].scripts as script}<script
+        src={script.src}
+        async={!!script.async}
+        defer={!!script.async}>
+      </script>{/each}
   {/if}
-  {#if config['status-website'].links}
-    {#each config['status-website'].links as link}
+  {#if config["status-website"].links}
+    {#each config["status-website"].links as link}
       <link rel={link.rel} href={link.href} media={link.media} />
     {/each}
   {/if}
+  <link
+    rel="stylesheet"
+    href={`/themes/${(config["status-website"] || {}).theme || "light"}.css`}
+  />
   {#if config['status-website'].css}
-    {@html `
-    <style>
-${config['status-website'].css}
-    </style>`
-    }
+    {@html `<style>${config['status-website'].css}</style>`}
   {/if}
   {#if config['status-website'].js}
-    {@html `
-    <script>
-${config['status-website'].js}
-    </script>`
-    }
+    {@html `<script>${config['status-website'].js}</script>`}
   {/if}
 </svelte:head>
 
@@ -58,6 +50,16 @@ ${config['status-website'].js}
 
 <footer>
   <p>
-    {@html snarkdown(config.i18n.footer.replace(/\$REPO/, `https://github.com/${config.owner}/${config.repo}`))}
+    {@html snarkdown(
+      config.i18n.footer.replace(/\$REPO/, `https://github.com/${config.owner}/${config.repo}`)
+    )}
   </p>
 </footer>
+
+<style>
+  footer {
+    text-align: center;
+    opacity: 0.75;
+    margin-top: 3rem;
+  }
+</style>
