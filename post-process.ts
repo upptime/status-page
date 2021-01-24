f99f9650bf0ff84d33b16fb0ca6bc0bc59931332
Anand Chowdhary <mail@anandchowdhary.com>
2021-01-24 13:38:43 +0530

    :recycle: Change static -> assets

--- post-process.ts
+++ post-process.ts
@@ -22,8 +22,8 @@ export const postProcess = async () => {
     await remove(join(".", "__sapper__", "export", baseUrl));
   }
 
-  if (pathExists(join(".", "static")))
-    await copy(join(".", "static"), join("__sapper__", "export"), { recursive: true });
+  if (pathExists(join(".", "assets")))
+    await copy(join(".", "assets"), join("__sapper__", "export"), { recursive: true });
 
   if (config["status-website"] && config["status-website"].cname)
     await writeFile(join(".", "__sapper__", "export", "CNAME"), config["status-website"].cname);
